df44adb0e34198b69badc67e8901cd44ef7570da
SanghunLee-anu <ac83730@naver.com>
2016-01-23 00:31:43 +0900

    Update test_ripple_carry_counter.v

--- test_ripple_carry_counter.v
+++ test_ripple_carry_counter.v
@@ -1,1 +1,44 @@
+`timescale 1ns / 1ps
+
+
+module test;
+
+	// Inputs
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire [3:0] q;
+
+	// Instantiate the Unit Under Test (UUT)
+	ripple_carry_counter uut (
+		.q(q), 
+		.clk(clk), 
+		.reset(reset)
+	);
+	
+	always
+			#5 clk=~clk;
+
+	initial 
+		begin
+			clk = 0;
+			reset = 0;
+		end
+	
+	
+	initial 	
+		begin
+			reset = 1'b1;
+			#15 reset = 1'b0;
+			#180 reset = 1'b1;
+			#10 reset = 1'b0;
+			#20 $finish;
+		end
+			
+			
+	initial
+			$monitor($time,"output q =%d",q);
+	
+endmodule
 
